c4eadf4d7e9afaa12da0269efafd105459cd10c4
Jacob Karlsson <jacob.karlsson95@gmail.com>
2016-08-13 23:07:43 +0200

    Improve missing file handling when listing

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use std::io;
 use std::io::Read;
-use std::fs::File;
+use std::io::ErrorKind;
+use std::fs::OpenOptions;
 
 fn main() {
     match list_content() {
-        Ok(()) => println!("Listed contents successfully."),
-        Err(e) => println!("Error listing contents: {:?}", e)
+        Ok(()) => (),
+        Err(e) => 
+            match e.kind() {
+                ErrorKind::NotFound => println!("todo.txt file not found"),
+                _ => println!("Other error with listing")
+            }
     }
 }
 
 fn list_content() -> io::Result<()> {
     //read file and print it
-    let mut file = try!(File::open("todo.txt"));
+    let mut file = try!(
+        OpenOptions::new()
+        .read(true)
+        .open("todo.txt"));
     let mut content = String::new();
     match file.read_to_string(&mut content) {
         Ok(_) => print!("{}", content),
