f589f1ea74388128b73e9ec328dd5111248dc349
Jacob Karlsson <jacob.karlsson95@gmail.com>
2016-08-13 16:31:15 +0200

    Add basic test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+pub fn hi() -> &'static str {
+    return "hi";
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_test() {
+		assert_eq!(hi(), "hi");
+    }
+}
