c4d6474edef1f4bbd130a54ca392085b6d672b73
Jacob Karlsson <jacob.karlsson95@gmail.com>
2016-08-13 18:52:43 +0200

    Make it read the todo.txt file
    The program is a fucking mess so far but we're getting somewhere.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use std::io;
+use std::io::Read;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    match list_content() {
+        Ok(()) => println!("Listed contents successfully."),
+        Err(e) => println!("Error listing contents: {:?}", e)
+    }
+}
+
+fn list_content() -> io::Result<()> {
+    //read file and print it
+    let mut file = try!(File::open("todo.txt"));
+    let mut content = String::new();
+    match file.read_to_string(&mut content) {
+        Ok(_) => print!("{}", content),
+        Err(e) => println!("Error reading file: {:?}", e)
+    }
+    Ok(())
 }
 
 pub fn hi() -> &'static str {
